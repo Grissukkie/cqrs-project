9af70ccd2c42a02745ff75d5c61ec87f1e5b8bbb
Grissukkie <val.ladino4@gmail.com>
2025-02-25 21:40:59 -0600

    Create functional

--- app/commands/patients_command.py
+++ app/commands/patients_command.py
@@ -3,11 +3,11 @@ from app.models.patients import PatientDB
 from app.schemas.patients_schema import PatientCreate
 
 class PatientCommandHandler:
-  def __init__(self, db_session):
-    self.db_session = db_session
+    def __init__(self, db_session):
+        self.db_session = db_session
 
     async def create_patient(self, patient: PatientCreate):
-      qInsert = insert(PatientDB).values(patient.model_dump())
-      await self.db_session.execute(qInsert)
-      await self.db_session.commit()
-      return{"message": "Paciente agregado exitosamente."}
+        qInsert = insert(PatientDB).values(patient.model_dump())
+        await self.db_session.execute(qInsert)
+        await self.db_session.commit()
+        return {"message": "Paciente agregado exitosamente."}
